a7b589f19696d03679b058312fda299aee3faa3f
Jon Love <jon_w-l@hotmail.com>
2025-01-07 12:00:46 +0800

    Add animations for right and wrong

--- src/pages/GuessingPage.jsx
+++ src/pages/GuessingPage.jsx
@@ -5,6 +5,7 @@ import {
   getPreviouslyFoundAnswer,
 } from '../services/apiServices'
 import Button from '../components/Button'
+import { DotLottieReact } from '@lottiefiles/dotlottie-react'
 
 const GuessingPage = () => {
   const id = useParams().id
@@ -13,6 +14,7 @@ const GuessingPage = () => {
   const [names, setNames] = useState([])
   const [question, setQuestion] = useState()
   const [error, setError] = useState(null)
+  const [isCorrect, setIsCorrect] = useState(undefined)
 
   useEffect(() => {
     const getResults = async () => {
@@ -23,7 +25,6 @@ const GuessingPage = () => {
         } else {
           results = await getARandomAnswerWithNames(id)
         }
-        console.log(results)
         setNames(results.names)
         setQuestion(results.question)
         setSearchParams({ prev: results.question?.id })
@@ -35,8 +36,33 @@ const GuessingPage = () => {
     getResults()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id])
+
+  const handleClick = id => {
+    const isCorrect = question.id === id
+    setIsCorrect(isCorrect)
+  }
+
   return (
     <div className='centered column oneHundred aligned'>
+      {isCorrect && (
+        <div id='isCorrect' className='row centered aligned'>
+          <DotLottieReact
+            src='https://lottie.host/e66c5850-83d0-41f5-b7c0-7a0c81da4afb/LoWAsv99Tg.lottie'
+            loop={false}
+            autoplay
+            speed={1}
+          />
+        </div>
+      )}
+      {isCorrect === false && (
+        <div id='isFalse' className='row centered aligned'>
+          <DotLottieReact
+            src='https://lottie.host/04919c23-1007-4aef-b8f6-7c144793be2a/sDKPmJTFg0.lottie'
+            loop={false}
+            autoplay
+          />
+        </div>
+      )}
       <h1>Guess who said:</h1>
       {question && <h2 style={{ color: 'blue' }}>{question.answer}</h2>}
       <div className='row centered aligned' style={{ gap: 10, overflow: 'scroll' }}>
@@ -46,6 +72,8 @@ const GuessingPage = () => {
               style={{ backgroundColor: 'blue', color: 'white' }}
               key={item.id}
               text={item.name}
+              onClick={() => handleClick(item.id)}
+              disabled={isCorrect !== undefined}
             />
           ))}
       </div>
